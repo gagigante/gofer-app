6d481116602aa49c7eda752352d93c3f41d8297f
Gabriel Gigante <gabriel.gigante@isaac.com.br>
2024-05-16 22:11:18 -0300

    chore: adjusting window size

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,10 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    minWidth: 800,
-    minHeight: 600,
+    width: 1080,
+    height: 720,
+    minWidth: 1080,
+    minHeight: 720,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
