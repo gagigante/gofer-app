3001997a7ef6729171b3dfd0f817c21a7f4cf87b
Gabriel Gigante <gabriel.gigante@isaac.com.br>
2024-05-22 16:47:49 -0300

    feat: creates theme provider

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router-dom'
 
 import { AuthProvider } from './contexts/AuthContext'
 import { ToastContext } from './contexts/ToastContext'
+import { AuthProvider } from './contexts/AuthContext'
 import { router } from './routes'
 
 import '@/view/styles/global.css'
@@ -12,9 +13,11 @@ const divElement = document.getElementById('root')!
 const root = createRoot(divElement)
 
 root.render(
-  <ToastContext>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  </ToastContext>,
+  <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+    <ToastContext>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </ToastContext>
+  </ThemeProvider>,
 )
