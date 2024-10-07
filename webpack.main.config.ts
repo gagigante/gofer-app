89486293478655856efdbfc84d6ac9af364a301a
Gabriel Gigante <gabriel.gigante@isaac.com.br>
2024-10-06 23:14:36 -0300

    feat: replace vite for electron-forge webpack template (#13)
    * feat: setup webpack
    
    * feat: setup envs var

---
+++ webpack.main.config.ts
@@ -0,0 +1,31 @@
+import path from 'path'
+import type { Configuration } from 'webpack'
+import Dotenv from 'dotenv-webpack'
+
+import { rules } from './webpack.rules'
+import { plugins } from './webpack.plugins'
+
+export const mainConfig: Configuration = {
+  /**
+   * This is the main entry point for your application, it's the first file
+   * that runs in the main process.
+   */
+  entry: './src/main.ts',
+  // Put your normal webpack config below here
+  module: {
+    rules,
+  },
+  externals: {
+    '@libsql/client': 'commonjs @libsql/client'
+  },
+  plugins: [
+    new Dotenv(),
+    ...plugins
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
+  },
+};
