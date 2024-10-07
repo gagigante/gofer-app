b77d0ca47dea8b4d81c46e60c4ef9ba3d02937e7
Gabriel Gigante <gabrielgigante29@gmail.com>
2024-10-06 23:29:55 -0300

    chore: migrates users repository

--- src/api/repositories/users-repository.ts
+++ src/api/repositories/users-repository.ts
@@ -1,83 +1,69 @@
-import { type User } from '@prisma/client'
-import { type UserRole } from '../types/user-role'
+import { asc, eq, like, count } from 'drizzle-orm'
 
-import { prisma } from '../db/client'
+import { db } from '@/api/db/client'
+import { type NewUser, type User, users } from '../db/schema'
+
+import { type UserRole } from '../types/user-role'
 
 export class UsersRepository {
   public async getUserById(userId: string): Promise<User | null> {
-    const user = await prisma.user.findUnique({
-      where: { id: userId },
-    })
+    const response = await db.select().from(users).where(eq(users.id, userId)).get()
 
-    return user
+    return response ?? null
   }
 
   public async getUserByName(name: string): Promise<User | null> {
-    const user = await prisma.user.findUnique({
-      where: { name },
-    })
+    const response = await db.select().from(users).where(eq(users.name, name)).get()
 
-    return user
+    return response ?? null
   }
 
-  public async getUsers(name = '', page = 1, itemsPerPage = 15): Promise<User[]> {
-    const users = await prisma.user.findMany({
-      where: {
-        name: {
-          contains: name,
-        },
-      },
-      take: itemsPerPage,
-      skip: page === 1 ? 0 : page,
-    })
+  public async getUsers(name = '', page = 1, itemsPerPage = 4): Promise<User[]> {
+    const response = await db
+      .select()
+      .from(users)
+      .where(like(users.name, `%${name}%`))
+      .orderBy(asc(users.name))
+      .offset(page === 1 ? 0 : (page - 1) * itemsPerPage)
+      .limit(itemsPerPage)
 
-    return users
+    return response
   }
 
   public async countUsers(name = ''): Promise<number> {
-    const usersCount = await prisma.user.count({
-      where: {
-        name: {
-          contains: name,
-        },
-      },
-    })
+    const [response] = await db.select({ count: count() }).from(users).where(like(users.name, `%${name}%`))
 
-    return usersCount
+    return response.count
   }
 
-  public async createUser({ id, name, password, role }: User & { role: UserRole }): Promise<User> {
-    const user = await prisma.user.create({
-      data: {
+  public async createUser({ id, name, password, role }: NewUser & { role: UserRole }): Promise<User> {
+    const [response] = await db
+      .insert(users)
+      .values({
         id,
         name,
         password,
         role,
-      },
-    })
+      })
+      .returning()
 
-    return user
+    return response
   }
 
   public async deleteUser(userId: string): Promise<void> {
-    await prisma.user.delete({
-      where: {
-        id: userId,
-      },
-    })
+    await db.delete(users).where(eq(users.id, userId))
   }
 
-  public async updateUser(userId: string, name: string, newPassword?: string): Promise<User> {
-    const user = await prisma.user.update({
-      where: {
-        id: userId,
-      },
-      data: {
+  public async updateUser({ id, name, password }: Pick<User, 'id' | 'name'> & { password?: string }): Promise<User> {
+    const [response] = await db
+      .update(users)
+      .set({
         name,
-        password: newPassword,
-      },
-    })
+        password,
+      })
+      .where(eq(users.id, id))
+      .returning()
 
-    return user
+    return response
   }
 }
