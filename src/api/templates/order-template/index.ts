af63ac084705edbc0637d6e200c8dfbed4681744
Gabriel Gigante <gabriel.gigante@isaac.com.br>
2024-11-05 18:08:43 -0300

    feat: creates get order template function

--- src/api/templates/order-template/index.ts
+++ src/api/templates/order-template/index.ts
@@ -1,32 +1,61 @@
 import path from 'node:path'
 import { open } from 'node:fs/promises'
 import { app } from 'electron'
+import Handlebars from 'handlebars'
 
-export async function getOrderTemplate() {
-  // src / api / templates / order - template.html
-  // Read the HTML template file
+import { formatCurrency } from '@/view/utils/formatters'
+import { parseCentsToDecimal } from '@/view/utils/parsers'
+
+import { type Response } from '@/api/types/response'
+
+interface Data {
+  id: string
+  totalPrice: number | null
+  createdAt: string | null
+  products: Array<{
+    productId: string | null
+    quantity: number | null
+    price: number | null
+    name: string | null
+    barCode: string | null
+    fastId: number | null
+  }>
+}
+
+export async function getOrderTemplate(data: Data): Promise<Response<string>> {
   const templatePath = path.join(app.getAppPath(), 'src', 'api', 'templates', 'order-template', 'index.html')
-  const template = await open(templatePath)
-  // let template = fs.readFile(templatePath, {})
 
-  // Replace placeholders with order data
-  // template = template
-  //   .replace('{{orderId}}', order.id)
-  //   .replace('{{orderDate}}', order.date)
-  //   .replace('{{customerName}}', order.customerName)
-  //   .replace('{{total}}', order.total.toFixed(2))
+  try {
+    const file = await open(templatePath)
+    const fileBuf = await file.readFile()
 
-  // Generate item rows as HTML
-  // const itemsHtml = order.items
-  //   .map(
-  //     (item) => `
-  //     <div class="item-row">
-  //       <span>${item.name} (x${item.quantity})</span>
-  //       <span>$${(item.price * item.quantity).toFixed(2)}</span>
-  //     </div>`,
-  //   )
-  //   .join('')
+    const template = fileBuf.toString()
+    const handlebarsTemplate = Handlebars.compile<{
+      products: ReturnType<typeof formatProducts>
+      orderTotal: string
+    }>(template)
 
-  // template = template.replace('{{items}}', itemsHtml)
-  return template
+    await file.close()
+
+    const formattedProducts = formatProducts(data.products)
+    const formattedOrderTotal = formatCurrency(parseCentsToDecimal(data.totalPrice ?? 0))
+
+    return {
+      data: handlebarsTemplate({ products: formattedProducts, orderTotal: formattedOrderTotal }),
+      err: null,
+    }
+  } catch (err) {
+    return { data: null, err: err as Error }
+  }
+}
+
+function formatProducts(data: Data['products']) {
+  return data.map((item) => {
+    return {
+      ...item,
+      barCode: item.barCode || 'N/A',
+      price: formatCurrency(parseCentsToDecimal(item.price ?? 0)),
+      total: formatCurrency(parseCentsToDecimal((item.price ?? 0) * (item.quantity ?? 0))),
+    }
+  })
 }
