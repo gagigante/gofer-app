32d59b526c71f59b93c597369ff874ec4ee90808
Gabriel Gigante <gabriel.gigante@isaac.com.br>
2024-11-05 18:20:15 -0300

    feat: integrates download on client

--- src/view/pages/Orders/components/OrdersTable.tsx
+++ src/view/pages/Orders/components/OrdersTable.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/view/components/ui/table'
-import { FaEye } from 'react-icons/fa'
+import { FaEye, FaFile } from 'react-icons/fa'
 
 import { Button } from '@/view/components/ui/button'
 import { OrdersDetailsDialog } from './OrdersDetailsDialog'
 
+import { useAuth } from '@/view/hooks/useAuth'
+import { useToast } from '@/view/components/ui/use-toast'
+
 import { formatCurrency } from '@/view/utils/formatters'
 import { parseCentsToDecimal } from '@/view/utils/parsers'
 
+import { type OrdersApi, apiName } from '@/api/exposes/orders-api'
 import { type Order } from '@/api/db/schema'
 
 interface OrdersTableProps {
@@ -17,8 +21,34 @@ interface OrdersTableProps {
 const FORMATTER = new Intl.DateTimeFormat('pt-BR', { dateStyle: 'medium', timeStyle: 'short' })
 
 export function OrdersTable({ orders }: OrdersTableProps) {
+  const { toast } = useToast()
+  const { user } = useAuth()
   const [selectedOrder, setSelectedOrder] = useState<string>()
   const [isOrderDetailsDialogOpen, setIsOrderDetailsDialogOpen] = useState(false)
+
+  async function handleSaveOrderFile(orderId: string) {
+    if (!user) return
+
+    const { data, err } = await (window as unknown as Record<typeof apiName, OrdersApi>).ordersApi.downloadFile({
+      loggedUserId: user.id,
+      orderId,
+    })
+
+    if (err) {
+      toast({
+        title: 'Houve um erro ao tentar gerar o arquivo. Tente novamente.',
+        duration: 3000,
+      })
+      return
+    }
+
+    if (data?.is_canceled) return
+
+    toast({
+      title: 'Arquivo salvo com sucesso.',
+      duration: 3000,
+    })
+  }
 
   return (
     <>
@@ -54,6 +84,16 @@ export function OrdersTable({ orders }: OrdersTableProps) {
                   }}
                 >
                   <FaEye className="w-3 h-3" />
+                </Button>
+
+                <Button
+                  variant="outline"
+                  size="sm"
+                  onClick={async () => {
+                    await handleSaveOrderFile(id)
+                  }}
+                >
+                  <FaFile className="w-3 h-3" />
                 </Button>
               </TableCell>
             </TableRow>
