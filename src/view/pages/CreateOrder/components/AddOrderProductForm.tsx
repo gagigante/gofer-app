202985043bc9928f72ffd027397f2b60c0960336
Gabriel Gigante <gabriel.gigante@isaac.com.br>
2024-11-23 00:08:53 -0300

    feat: updartes crete order workflow

--- src/view/pages/CreateOrder/components/AddOrderProductForm.tsx
+++ src/view/pages/CreateOrder/components/AddOrderProductForm.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
-import { Dialog } from '@/view/components/Dialog'
 import { Input } from '@/view/components/ui/input'
 import { Combobox } from '@/view/components/Combobox'
 
@@ -9,20 +8,25 @@ import { useAuth } from '@/view/hooks/useAuth'
 
 import { type Product } from '@/api/db/schema'
 import { type ProductWithCategoryAndBrand } from '@/api/repositories/products-repository'
+import { Button } from '@/view/components/ui/button'
+import { Label } from '@radix-ui/react-label'
 
-interface AddOrderProductDialogProps {
+interface ProductOption {
+  label: string
+  value: string
+}
+
+interface AddOrderProductFormProps {
   preSelectedProduct: ProductWithCategoryAndBrand | null
-  isOpen: boolean
-  onClose: () => void
   onSubmit: (product: Product, quantity: number) => void
 }
 
-// TODO: deve validar se ha quantidade suficiente
-export function AddOrderProductDialog({ preSelectedProduct, isOpen, onClose, onSubmit }: AddOrderProductDialogProps) {
+export function AddOrderProductForm({ preSelectedProduct, onSubmit }: AddOrderProductFormProps) {
   const { user } = useAuth()
+  const quantityInputRef = useRef<HTMLInputElement>(null)
 
   const [filter, setFilter] = useState('')
-  const [product, setProduct] = useState<{ label: string; value: string }>()
+  const [product, setProduct] = useState<ProductOption>()
   const [quantity, setQuantity] = useState(0)
 
   const { data: productsResponse } = useProducts(
@@ -42,40 +46,28 @@ export function AddOrderProductDialog({ preSelectedProduct, isOpen, onClose, onS
     if (preSelectedProduct) {
       setProduct({ label: preSelectedProduct.name ?? '', value: preSelectedProduct.id })
       setQuantity(1)
+      quantityInputRef.current?.focus()
     }
   }, [preSelectedProduct])
 
-  useEffect(() => {
-    if (!preSelectedProduct || !isOpen) {
-      setProduct(undefined)
-      setQuantity(0)
-    }
-  }, [isOpen])
-
-  function handleSubmit() {
-    if (!product) return
+  function handleAddProduct(product: ProductOption | undefined, quantity: number) {
+    if (!product || quantity === 0) return
 
     const selectedProduct = productsResponse?.products.find((item) => item.id === product.value)
 
     if (selectedProduct) {
       onSubmit(selectedProduct, quantity)
-      onClose()
+      setFilter('')
+      setProduct(undefined)
+      setQuantity(1)
     }
   }
 
   return (
-    <Dialog
-      title="Adicionar produto ao pedido"
-      onProceed={handleSubmit}
-      proceedButtonLabel="Adicionar produto"
-      isProceedButtonDisabled={quantity === 0 || !product}
-      cancelButtonLabel="Cancelar"
-      open={isOpen}
-      onClose={() => {
-        onClose()
-      }}
-    >
-      <div className="flex items-center gap-2">
+    <div className="w-full flex gap-4 items-end">
+      <div className="flex-1 w-full flex flex-col gap-2">
+        <Label>Produto</Label>
+
         <Combobox
           placeholder="Selecione um produto"
           searchPlaceholder="Pesquisar por nome de produto"
@@ -85,8 +77,13 @@ export function AddOrderProductDialog({ preSelectedProduct, isOpen, onClose, onS
           onSelectOption={setProduct}
           onChangeFilter={setFilter}
         />
+      </div>
+
+      <div className="w-[140px] flex flex-col gap-2">
+        <Label>Quantidade</Label>
 
         <Input
+          ref={quantityInputRef}
           className="w-full"
           type="number"
           min={0}
@@ -94,6 +91,10 @@ export function AddOrderProductDialog({ preSelectedProduct, isOpen, onClose, onS
           onChange={(e) => setQuantity(Number(e.target.value))}
         />
       </div>
-    </Dialog>
+
+      <Button onClick={() => handleAddProduct(product, quantity)} disabled={!product || quantity === 0}>
+        Adicionar produto
+      </Button>
+    </div>
   )
 }
