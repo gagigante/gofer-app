29c4eecb2c0fe331599ba0ac35172f4a32931159
Gabriel Gigante <gabriel.gigante@isaac.com.br>
2025-01-05 01:23:12 -0300

    feat: setup test client

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,19 @@
 import 'dotenv/config'
 import { defineConfig } from 'drizzle-kit'
 
+const dbCredentials =
+  process.env.NODE_ENV === 'test'
+    ? {
+        url: 'file:test.db',
+      }
+    : {
+        url: process.env.TURSO_DATABASE_URL!,
+        authToken: process.env.TURSO_AUTH_TOKEN,
+      }
+
 export default defineConfig({
   dialect: 'turso',
   schema: './src/api/db/schema.ts',
   out: './migrations',
-  dbCredentials: {
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN,
-  },
+  dbCredentials,
 })
--- src/api/db/client.ts
+++ src/api/db/client.ts
@@ -1,8 +1,13 @@
-import { drizzle } from 'drizzle-orm/libsql/web'
+import { drizzle } from 'drizzle-orm/libsql'
 
-export const db = drizzle({
-  connection: {
-    url: `${process.env.TURSO_DATABASE_URL}`,
-    authToken: `${process.env.TURSO_AUTH_TOKEN}`,
-  },
-})
+const connection =
+  process.env.NODE_ENV === 'test'
+    ? {
+        url: 'file:test.db',
+      }
+    : {
+        url: process.env.TURSO_DATABASE_URL!,
+        authToken: process.env.TURSO_AUTH_TOKEN,
+      }
+
+export const db = drizzle({ connection })
