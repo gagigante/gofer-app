bf72f5ec3c46beeef567201abb4dcee6b99827d2
Gabriel Gigante <gabriel.gigante@isaac.com.br>
2025-01-16 23:41:04 -0300

    feat: adjusts order-report to shows custom price product

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 
+import { TooltipProvider } from '@/view/components/ui/tooltip'
 import { ReactQueryContext } from './contexts/ReactQueryContext'
 import { ThemeProvider } from './contexts/ThemeContext'
 import { ToastContext } from './contexts/ToastContext'
@@ -16,11 +16,13 @@ const root = createRoot(container)
 root.render(
   <ReactQueryContext>
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <ToastContext>
-        <AuthProvider>
-          <RouterProvider router={router} />
-        </AuthProvider>
-      </ToastContext>
+      <TooltipProvider>
+        <ToastContext>
+          <AuthProvider>
+            <RouterProvider router={router} />
+          </AuthProvider>
+        </ToastContext>
+      </TooltipProvider>
     </ThemeProvider>
   </ReactQueryContext>,
 )
