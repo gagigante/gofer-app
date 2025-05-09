fff50b49d00177efc8094174863b8b54e8a0aa96
Gabriel Gigante <gabrielgigante29@gmail.com>
2025-05-09 09:44:28 -0300

    feat: adds address attributes to orders entity

---
+++ migrations/0017_unique_toad_men.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `orders` ADD `zipcode` text;--> statement-breakpoint
+ALTER TABLE `orders` ADD `city` text;--> statement-breakpoint
+ALTER TABLE `orders` ADD `street` text;--> statement-breakpoint
+ALTER TABLE `orders` ADD `neighborhood` text;--> statement-breakpoint
+ALTER TABLE `orders` ADD `complement` text;
