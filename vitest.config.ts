cc2c37717f69c1899b4f8a293d424a9ba18d63a3
Gabriel Gigante <gabrielgigante29@gmail.com>
2026-02-06 23:30:03 -0300

    feat: adjusts vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,20 +1,30 @@
 import path from 'node:path'
+import { loadEnv } from 'vite'
 import { defineConfig } from 'vitest/config'
 
-export default defineConfig({
-  test: {
-    fileParallelism: false,
-    setupFiles: ['./vitest-setup.ts'],
-    coverage: {
-      include: ['src/**/*.ts', 'src/**/*.tsx'],
-      exclude: ['src/view', 'src/api/db', 'src/api/env', 'src/api/db/migrations', 'src/api/db/seeders'],
-      provider: 'v8',
-      reporter: ['text', 'json', 'html', 'lcov'],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    test: {
+      fileParallelism: false,
+      setupFiles: ['./vitest-setup.ts'],
+      env: {
+        ...env,
+        ENV: 'test',
+      },
+      coverage: {
+        include: ['src/**/*.ts', 'src/**/*.tsx'],
+        exclude: ['src/view', 'src/api/db', 'src/api/env', 'src/api/db/migrations', 'src/api/db/seeders'],
+        provider: 'v8',
+        reporter: ['text', 'json', 'html', 'lcov'],
+      },
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+  }
 })
