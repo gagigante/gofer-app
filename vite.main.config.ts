685ed2ed351e52479df64830aadd540733db6162
Gabriel Gigante <gabrielgigante29@gmail.com>
2026-02-07 00:28:54 -0300

    fix: fixes publish

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config
 export default defineConfig(({ mode }) => {
