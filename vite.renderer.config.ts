a1be63dc0d48a25bfa6cb836f01252db54581bb1
Gabriel Gigante <gabrielgigante29@gmail.com>
2026-02-06 23:59:40 -0300

    fix: removes envs from client

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,22 +1,13 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    define: {
-      // Make env variables available as process.env in the browser
-      'process.env': JSON.stringify(env),
-    },
-  }
+  },
 })
